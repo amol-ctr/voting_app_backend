17a9ed4fd490b8fb476ebf58d75c0d70a1700653
AMOL MITTAL <58140879+amol-ctr@users.noreply.github.com>
2024-09-27 10:05:32 +0530

    Delete auth.js

--- auth.js
+++
@@ -1,8 +0,0 @@
-const passport=require('passport');
-const LocalStrategy=require('passport-local').Strategy;
-
-const express=require('express');
-
-const User=require('./models/user');
-
-passport.use()
